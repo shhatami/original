f2321cefbf55dd3799a3217ae9ac4f01e2307ed7
hatami <53007257+shhatami@users.noreply.github.com>
2022-09-25 17:11:57 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,395 @@
+<!--main page-->
+<!DOCTYPE html>
+<html lang="fa" lang="en" dir="rtl"> <head>
+    <meta charset="UTF-8">
+
+<title>اوگوفای: سایت خرید تور</title>
+
+    <link rel="icon" type="image/x-icon" href="http://i66.tinypic.com/2m5ff5j.png">
+    
+    <meta name="keywords" content="ogofy,اوگوفای,offer,تخفیف"/>
+    <meta name="discription" content="خرید تور,خرید تورآنلاین,تخفیف توخفیف,ر,اینترنتی,travel,سفر"/>
+    
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tahoma">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <style type="text/css">
+        
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+li {
+  float: left;
+}
+
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+ 
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+        
+        /*i can not play it*/
+        div[content]
+        {
+        background-color: lightgrey;
+        width: 300px;
+        border: 15px solid green;
+        padding: 50px;
+        margin: 20px;
+        }
+        h1,h2,h3,h4,h5,h6{font-family:Lato, Tahoma, sans-serif; font-size:30px;}
+       .w3-bar,h1,button {font-family: Lato, Tahoma, Montserrat, sans-serif; font-size:18px;}
+       .fa-anchor,.fa-coffee {font-size:200px}
+        body
+        {
+        margin:0;
+        font-family:Lato, Tahoma, sans-serif; font-size:15px;
+        }
+        .header
+        {
+        background-color: white;
+        padding:40px;
+        text-align: center;
+        }
+        html
+        {
+        overflow-y: scroll;
+        }
+        .page
+        { 
+        background-color: blue !important;
+        background-color: ghostwhite;
+        }
+        #container
+        {
+        /* all browsers except ie6 will respect the !important flag */
+        height: auto !important;
+        min-height: 500px;
+        /* Should have the same value as the min height above */
+        height: 500px;
+        }
+        a[target="_blank"]:before,
+        a[target="new"]:before 
+        {
+        margin: 0 5px 0 0;
+        padding: 1px;
+        outline: 1px solid #333;
+        color: #333;
+        background: #ff9;
+        font: 12px "Zapf Dingbats";
+        content: "\279C";
+        }
+        .content
+        {
+        position:absolute;
+        text-align:center;
+        overflow:auto;
+        min-height:300px;
+        max-height:1280px;
+        padding:10px;
+        top:47px;
+        right:0px;
+        left:0px;
+        background-color:hsla(9, 100%, 64%, 0.5);
+        width: 1525px;
+        height: 500px;
+        }
+        .subcontent
+        {
+        position:absolute;
+        text-align:center;
+        overflow:auto;
+        min-height:200px;
+        max-height:400px;
+        padding:10px;
+        top:546px;
+        right:0px;
+        left:0px;
+        background-color:#f0f0f0;
+        width: 1550px;
+        height: 500px;
+        }
+        .subsubcontent
+        {
+        position:absolute;
+        text-align:left;
+        overflow:auto;
+        min-height:200px;
+        max-height:400px;
+        padding:10px 80px 0px 80px;
+        top:945px;
+        right:0px;
+        left:0px;
+        background-color:#f0f0f0;
+        width: 1550px;
+        height:300px;
+        }
+        /* Create three equal columns that floats next to each other */
+        .column
+        {
+        float: left;
+        width: 33.33%;
+        padding: 15px;
+        }
+        .footer
+        {
+        position:absolute;
+        background-color:#787878;
+        text-align:rtl;
+        right:0px;
+        left:0px;
+        height:250px;
+        margin-top:1200px;
+        width:1600px;
+        font-family:sans-serif;
+        font-size:16px;
+        padding:10px 80px 0px 80px;
+        }
+
+</style>
+    
+</head>
+     
+<body>      
+         
+<div id="header">
+     
+        <p></p>
+        <!-- Navbar or header -->
+        <div class="w3-top">
+        <div class="w3-bar w3-white w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-gray w3-large w3-yellow" href="javascript:void(0);" onclick="myFunction()" 
+        title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#"><img src="http://i68.tinypic.com/rii55e.jpg" border="0" alt="Image and video hosting by TinyPic" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"></a>
+    <a href="http://localhost/faradars/original/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">خانه</a>
+    <a href="http://localhost/faradars/enter/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" target="_blank;">ورود</a>
+    <a href="http://localhost/faradars/remember1/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" target="_blank0;">ثبت نام</a>
+    <ul>
+        <li class="dropdown">
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" target="_blank1;" >ایرانگردی</a>
+    
+        
+    <div class="dropdown-content">
+      <a href="http://localhost/faradard/one-daye/index.php">تورهای یک روزه</a>
+      <a href="http://localhost/faradars/other-day/index.php">تورهای چندروزه</a>
+      
+    </div>
+    </li>
+    </ul>
+        </div>
+        <!-- Navbar on small screens -->
+        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="http://localhost/faradars/enter/index.php" class="w3-bar-item w3-button w3-padding-large">ورود</a>
+    <a href="http://localhost/faradars/remember1/index.php" class="w3-bar-item w3-button w3-padding-large">ثبت نام</a>
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-large" >ایرانگردی</a>
+    
+    <ul>
+
+        <li class="dropdown">
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" target="_blank1;" >ایرانگردی</a>
+    
+        
+    <div class="dropdown-content">
+      <a href="http://localhost/faradard/one-daye/index.php">تورهای یک روزه</a>
+      <a href="http://localhost/faradars/other-day/index.php">تورهای چندروزه</a>
+      
+    </div>
+    </li>
+    </ul>
+        </div>
+        </div>
+        </div>
+    
+   
+        <div class="content center" >
+           
+        <!-- Page content -->
+<div class="container" style="max-width:1500px;">
+
+        <div class="w3-display-right w3-padding w3-col l6 m8">
+        <div class="w3-container w3-yellow">
+        <h2><i class="fa fa-train w3-margin-right"></i></h2>
+        </div>
+        <div class="w3-container w3-white w3-padding-16">
+    
+        <div class="w3-row-padding" style="margin:0 -16px;">
+        <div class="w3-half w3-margin-bottom">
+            
+        <label><i class="fa fa-calendar-o"></i> تاریخ رفت</label>
+        <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="تاریخ رفت" required><br>
+        
+        </div>
+
+        <div class="w3-half">
+            
+        <label><i class="fa fa-calendar-o"></i> تاریخ برگشت</label>
+        <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="تاریخ برگشت" required><br>
+     
+        </div>
+        </div>
+    
+        <div class="w3-half">
+        <div class=" w3-margin-bottom" class="w3-row-padding w3-margin">
+        <input class="w3-input w3-border" type="text" placeholder="مقصد یا مکان مورد علاقه " name="distination" required>
+        </div>
+        <button class="w3-button w3-dark-grey" type="submit"><i class="fa fa-search w3-margin"></i>Search</button>
+        
+        </div>
+        </div>
+        </div>
+</div>
+        </div>        
+        
+        
+  
+<div class ="subcontent" class="w3-light-grey w3-content" style="max-width:1600px">
+    <p></p><br>
+    <a class="font_bold" href="#" title="">تورهایی که میزان علاقمندی مردم به آنها بیشتر بوده است</a>
+    
+    
+    
+
+
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-center:300px"><br>
+
+
+  
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">قشم</button><br>
+       
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">اصفهان</button><br>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">ساحل</button><br>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding">
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">ماسال</button><br>
+      </div>
+    </div>
+    <div class="w3-third w3-container w3-margin-bottom">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">شمال</button><br>
+      </div>
+    </div>
+    <div class="w3-third w3-container">
+      <img src="#.jpg" alt="#" style="width:100%" class="w3-hover-opacity">
+      <div class="w3-container w3-white"><br>
+        <p><b></b></p>
+        <button class="w3-button w3-block w3-grey w3-margin-bottom">شیراز</button><br>
+      </div>
+    </div>
+  </div>
+</div>
+</div> 
+        
+        <div class ="subsubcontent" class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-gray" style="text-align:left; font-size:15px;">بلاگ</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-gray" style="text-align:left; font-size:15px;">راهنما</a>
+    <p style="text-align:right; font-size:11px; text-indent: top; ">
+        همه ما به سفر نیاز داریم، سفری هر چند کوتاه.<br>
+        هر کسی به طریقی به دنبال یافتن یک مسیر خوب با قیمت مناسب در تاریخ مورد نظر<br>
+        خود میباشد. شرکت اوگوفای تصمیم دارد شما را در مسیر یافتن بهترینها همراهی نماید.<br>
+        این سایت با دیدگاه، ارائه تور با اهداف : قیمت مناسب، راحتی در سفر، اعتماد در خرید،<br>
+        ارائه بهترین ها، ایجاد شده است. تمامی خدمات این سایت،از طریق آژانس های باسابقه<br>
+        و مطمئن و دارای مجوز صورت گرفته است. در صورت هر گونه تخلف از طرف هریک از <br>
+        آنها مطابق قوانین در بخش فروش، خسارت پیش آمده، جبران خواهد شد.<br>
+        همراهی شما در سفر، افتخار ماست.<br>
+        </p>
+
+       
+
+        </div>
+  
+        <div>
+  
+      <footer class ="footer" style="text-align:center;" ><br><br><br><br><br>
+                  <i class="fa fa-instagram w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-twitter w3-margin w3-hover-opacity"></i>
+        <i class="fa fa-linkedin w3-margin w3-hover-opacity"></i>
+        <br>
+          <i class="fa fa-envelope w3-text-red w3-margin" style="width:10px;"> </i> Email: help@ogofy.com
+          <i class="fa fa-map-marker w3-text-red w3-margin" style="width:10px;"></i> ایران ، تهران، کرج
+       
+      
+
+  
+</footer>
+          </div>  
+  
+</body>
+     
+     
+     
+     
+     
+</html>
